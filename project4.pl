80b4e06dad8fc4b65f7daaf27baf8646ea7c2797
Jeremyg71089 <Jeremygalarza71089@gmail.com>
2016-05-09 12:07:09 -0700

    Fixed Some Errors

--- project4.pl
+++ project4.pl
@@ -2,7 +2,6 @@
 /*Jeremy Galarza*/
 /*Anderson Giang*/
 /*Eric Hernandez*/
-
 brother(X,Y):-male(X),parent(Z,X),parent(Z,Y).
 sister(X,Y):-female(X),parent(Z,X),parent(Z,Y).
 mother(X,Y):-female(X),parent(X,Y).
@@ -17,22 +16,27 @@ ancestor(X,Y):-parent(X,Y).
 ancestor(X,Y):-parent(X,Z),ancestor(Z,Y).
 descendant(X,Y):-parent(Y,X).
 descendant(X,Y):-parent(Y,Z),descendant(X,Z).
-relatives(X,Y):-(ancestor(Z,X),ancestor(Z,Y));(descendant(Z,X),descendant(Z,Y)).
+relatives(X,Y):-ancestor(Z,X),ancestor(Z,Y),not(X = Y).
+relatives(X,Y):-descendant(Z,X),descendant(Z,Y),not(X = Y).
+relatives(X,Y):-ancestor(X,Y).
+relatives(X,Y):-descendant(X,Y).
 
 f([X]):-number(X).
 f([X]):-char_type(X,lower).
 f(X):-(append(['('|L1],[')'],X),e(L1)).
 t(X):-f(X).
-t(X):-(append(L1,[*|L2],X),t(L1),f(L2));(append(L1,[/|L2],X),t(L1),f(L2)).
+t(X):-append(L1,[*|L2],X),t(L1),f(L2).
+t(X):-append(L1,[/|L2],X),t(L1),f(L2).
 e(X):-t(X).
-e(X):-(append(L1,[+|L2],X),e(L1),t(L2));(append(L1,[/|L2],X),e(L1),e(L2)).
+e(X):-append(L1,[+|L2],X),e(L1),t(L2).
+e(X):-append(L1,[-|L2],X),e(L1),t(L2).
 
 sum([],0).
 sum([H|T],X):- sum(T, Y),X is H + Y.
 subseq([], []).
 subseq([H|T], [H|R]) :- subseq(T,R).
 subseq([_|T], R) :- subseq(T,R).
-subseqsum(L,M,X):- subseq(L,Y),sum(Y,Z),M = Z,X = Y.
+subseqSum(L,M,X):- subseq(L,Y),sum(Y,Z),M = Z,X = Y.
 
 fsm(0,[a|T]):-fsm(0,T).
 fsm(0,[b|T]):-fsm(0,T).
@@ -40,5 +44,5 @@ fsm(0,[a|T]):-fsm(1,T).
 fsm(1,[b|T]):-fsm(2,T).
 fsm(2,[b|T]):-fsm(3,T).
 fsm(3,[a|T]):-fsm(3,T).
-fsm(3,[a|T]):-fsm(3,T).
+fsm(3,[b|T]):-fsm(3,T).
 fsm(3,[]).
